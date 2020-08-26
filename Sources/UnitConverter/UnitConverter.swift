27d2f04f97a8b3b78963130c3e72dfd3e2292a62
Stephanie <user.name>
2020-08-26 10:34:50 -0700

    update framework

--- Sources/UnitConverter/UnitConverter.swift
+++ Sources/UnitConverter/UnitConverter.swift
@@ -1,6 +1,6 @@
 // Just because the struct is public doesn't mean everything inside it is too. you must mark everything you want to be externally accessible with an access control level of public.
 
-public struct UnitConverter {
+public struct MyUnitConverter {
     
     private let secretNumber = 42
     
